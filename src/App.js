690e0c50b25e78580ee162a2e1fbbd2d691c6974
quanghoatrinh <quanghoa.trinh@gmail.com>
2023-11-27 09:00:29 +0700

    prepare layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Classroom from './components/Classroom';
+import NavComponent from './components/shared/nav';
 
 function App() {
   return (
     <div className="App">
-        <Classroom/>
+        <NavComponent/>
+        <div className='content'>
+
+        </div>
     </div>
   );
 }
